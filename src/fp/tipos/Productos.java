52db66a1f1251c7535d225fd392c2518d27a58e0
Javier Manrique <Javier.Manrique688@gmail.com>
2023-04-10 18:51:45 +0200

    Entrega 2.1

--- src/fp/tipos/Productos.java
+++ src/fp/tipos/Productos.java
@@ -1,5 +1,58 @@
 package fp.tipos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Productos {
 
+	private List<Producto> productos;
+	
+	public Productos(List<Producto> productos) {
+		this.productos = productos;
+	}
+	
+	public Productos() {
+		this.productos = new ArrayList();
+	}
+	
+	public List<Producto> getProductos() {
+		return productos;
+	}
+//a. obtener el número de elementos
+	public Integer getNumeroProductos() {
+		return productos.size();
+	}
+	
+//b. agregar un elemento
+	public void agregarProducto(Producto p) {
+		productos.add(p);
+	}
+	
+//c. agregar una coleccion de elementos
+	public void agregarConjuntoProductos(List<Producto> conj) {
+		productos.addAll(conj);
+	}
+	
+//d. eliminar un elemento por id
+	public void eliminarPorId(Integer numId) {
+		for(Producto p: productos) {
+			if(p.getId()==numId) {
+				productos.remove(p);
+			}
+		}
+	}
+	
+//2. contador
+	public Integer productosFueraStock() {
+		Integer contador = 0;
+		for(Producto p: productos) {
+			if(p.getStock() == false) {
+				contador++;
+			}
+			else {
+			}
+		}
+		return contador;
+	}
+	
 }
